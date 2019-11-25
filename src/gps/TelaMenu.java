617e9da86fc6ce65e5f012ede9c1daebdaeed7da
pardalito <57184300+pardalito@users.noreply.github.com>
2019-11-25 15:53:20 +0000

    again

--- src/gps/TelaMenu.java
+++ src/gps/TelaMenu.java
@@ -116,7 +116,7 @@ public class TelaMenu extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        new TelaLogin().setVisible(true);
+        //new TelaLogin().setVisible(true);
         this.setVisible(false);
     }//GEN-LAST:event_jButton2ActionPerformed
 
