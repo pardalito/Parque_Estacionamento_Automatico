3d675cf15b8ec832ce9e4bcd7c76378dd90d9a0b
pardalito <57184300+pardalito@users.noreply.github.com>
2019-11-28 17:29:43 +0000

    criaçao tela gestaoConta
    updtae da tela menu tambem mas ver como resolver isto que esta estranho

--- src/gps/TelaMenu.java
+++ src/gps/TelaMenu.java
@@ -120,8 +120,9 @@ public class TelaMenu extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        //new TelaLogin().setVisible(true);
-        this.setVisible(false);
+        TelaLogin login = new TelaLogin(this, rootPaneCheckingEnabled);
+        login.setVisible(true);
+        dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
